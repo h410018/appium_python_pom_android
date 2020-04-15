f04d769d42a2aa47c86d4449a524038bee37b3fd
Boyuan Huang <h410018@gmail.com>
2020-04-15 21:39:53 +0800

    change form

--- PageObject/HomePage.py
+++ PageObject/HomePage.py
@@ -12,17 +12,20 @@ from appium.webdriver.common.mobileby import MobileBy
 class HomePage:
     
     def __init__(self,driver):
+        
         self.driver = driver
-        self.login_btn = self.driver.find_element_by_id("com.zuvio.student:id/login_button")
-        self.register_btn = self.driver.find_element_by_id("com.zuvio.student:id/register_button")
-       
+        self.wait = WebDriverWait(self.driver,10)
+        self.elements_loc = {
+            "login_button" : 'com.zuvio.student:id/login_button',
+            "register_button" : 'com.zuvio.student:id/register_button'
+            }
     # Operations
     def enter_login_page(self):
         
-        WebDriverWait(self.driver,50).until(expected_conditions.presence_of_element_located((MobileBy.ID,'com.zuvio.student:id/login_button')))
+        self.login_btn = self.wait.until(expected_conditions.presence_of_element_located((By.ID,self.elements_loc["login_button"])))
         self.login_btn.click()
         
     def enter_register_page(self):
         
-        WebDriverWait(self.driver,10).until(expected_conditions.presence_of_element_located((By.ID,'com.zuvio.student:id/register_button')))
+        self.register_btn = self.wait.until(expected_conditions.presence_of_element_located((By.ID,self.elements_loc["register_button"])))
         self.register_btn.click()
--- PageObject/LoginPage.py
+++ PageObject/LoginPage.py
@@ -14,37 +14,41 @@ from selenium.webdriver.common.by import By
 
 class LoginPage:
     
+    elements_loc = {}  
     def __init__(self,driver):
         self.driver = driver
-        self.back_btn = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout[1]/android.view.ViewGroup/android.widget.ImageButton")
-        self.email = self.driver.find_element_by_id("com.zuvio.student:id/email_editText")
-        self.passwd = self.driver.find_element_by_id("com.zuvio.student:id/password_editText")
-        self.query_account = self.driver.find_element_by_id("com.zuvio.student:id/query_account_textView")
-        self.forget_password = self.driver.find_element_by_id("com.zuvio.student:id/forget_password_textView")
-        self.login_btn = self.driver.find_element_by_id("com.zuvio.student:id/login_button")
+        self.wait = WebDriverWait(self.driver,10)
+        self.elements_loc = {
+            "back_btn" : "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout[1]/android.view.ViewGroup/android.widget.ImageButton",
+            "email" : 'com.zuvio.student:id/email_editText',
+            "password" : 'com.zuvio.student:id/password_editText',
+            "query_password" : 'com.zuvio.student:id/query_account_textView',
+            "forget_password" : 'com.zuvio.student:id/forget_password_textView',
+            "login_button" : 'com.zuvio.student:id/login_button'
+            }
     
     def back_front_page(self):
-        WebDriverWait(self.driver,10).until(expected_conditions.presence_of_element_located((By.XPATH,"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout[1]/android.view.ViewGroup/android.widget.ImageButton")))
-        self.back_bin.click()
-    
+        self.back_btn =self.wait.until(expected_conditions.presence_of_element_located((By.XPATH,self.elements_loc["back_btn"])))
+        self.back_btn.click()
+        
     def input_email(self,email):
-        WebDriverWait(self.driver,10).until(expected_conditions.presence_of_element_located((By.ID,"com.zuvio.student:id/email_editText")))
+        self.email = self.wait.until(expected_conditions.presence_of_element_located((By.ID,self.elements_loc["email"])))
         self.email.clear()
         self.email.send_keys(email)  
         
-    def input_password(self,password):
-        WebDriverWait(self.driver,10).until(expected_conditions.presence_of_element_located((By.ID,"com.zuvio.student:id/password_editText")))
+    def input_password(self,password):    
+        self.passwd = self.wait.until(expected_conditions.presence_of_element_located((By.ID,self.elements_loc["password"])))
         self.passwd.clear()
         self.passwd.send_keys(password)
-    
+        
     def enter_query_password(self):
-        WebDriverWait(self.driver,10).until(expected_conditions.presence_of_element_located((By.ID,"com.zuvio.student:id/query_account_textView")))
+        self.query_account = self.wait.until(expected_conditions.presence_of_element_located((By.ID,self.elements_loc["query_password"])))
         self.query_account.click()
-    
+        
     def enter_forget_password(self):
-        WebDriverWait(self.driver,10).until(expected_conditions.presence_of_element_located((By.ID,"com.zuvio.student:id/forget_password_textView")))
-        self.forget_password.click()   
+        self.forget_password = self.wait.until(expected_conditions.presence_of_element_located((By.ID,self.elements_loc["forget_password"])))
+        self.forget_password.click() 
         
     def press_login_btn(self):
-        WebDriverWait(self.driver,10).until(expected_conditions.presence_of_element_located((By.ID,"com.zuvio.student:id/login_button")))
+        self.login_btn = self.wait.until(expected_conditions.presence_of_element_located((By.ID,self.elements_loc["login_button"])))
         self.login_btn.click()            
